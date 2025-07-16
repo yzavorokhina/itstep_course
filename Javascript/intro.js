bc963eaaa7d9cc47e219ad8891d1420b84ec80a3
Yuliya Zavorokhina <st@itstep.lan>
2025-07-16 21:47:10 +0300

    add JSIntro Lesson

---
+++ Javascript/intro.js
@@ -0,0 +1,102 @@
+
+//let message;
+
+//let message = 'Это сообщение';
+//let m1 = '123';
+//let $ = '123drefe';
+//let _ = '123dfrgr';
+
+//message = 'Что-то другое';
+
+//num = num + 10;
+
+//alert(message);
+
+//значение известно сначала
+//const COLOR = '#ffffff';
+
+//значение определяется позже
+//const color;
+
+//color = 2+3;
+
+//let firstSuperMessage = 'First message';
+
+//устаревшее
+//var name = 'dhcdhsvbdh';
+
+
+//Task1
+//let admin;
+//let name;
+//let admin, name;
+//name = 'Джон';
+//admin = name;
+//alert(admin);
+
+//Task2
+//let planetEarthName = 'Земля';
+//let currentUserOnline = {};
+
+//Task3
+//const Birthday ='18.04.1982'
+//const age = someCode(birthday);
+
+
+//variable types:
+//Number число отрицательные и положительные макс. для 64 битн.компьютеров
+//let age = 40;
+//BigInt
+//let hugeNumber = 2243434545454n;
+
+//alert (1/0); //Infinity
+//alert ('jfgirfjf'/2); //NaN - значение, а не тип
+//alert ('363jfgirfjf4543'/2); //6.63453453
+
+//string
+//let name = 'Edgar';
+//the same
+//name = "Edgar";
+//plus interpolation
+//name =`My name is Edgar. I'm ${age} years old`;
+//alert(name);
+
+//зарезервировать место для имени значение равно ничего
+//let money = null;
+
+//тип underfined без значения 
+//let happyDay;
+
+//object комплексный непримитивный тип, содержит несколько разных типов
+//let parameters = {};
+
+//age = 'ghfuuvhdfu';
+
+//булево значение / логическое
+//let nam = true; //false
+
+//тип символ
+//let $;
+
+//Оператор typeof
+
+//null as object in js but it is null
+//alert(typeof money);
+ 
+
+//Tasl
+//let name = "Ilya";
+
+//alert( `hello ${1}` ); // ?
+
+//alert( `hello ${"name"}` ); // ?
+
+//alert( `hello ${name}` ); // ?
+
+// let age = promt ('Введите свой возраст');
+// alert(`Ваш возраст ${age} лет`);
+
+let result = confirm('Вам уже есть 18 лет?');
+alert(result);
+
+//Task
---
+++ Javascript/math.js
@@ -0,0 +1,34 @@
+/*Task1*/
+// let userNumber = prompt('Введите какое-нибудь число');
+
+// let result = userNumber ** 2; // *userNumber;
+
+// alert(`Результат вычисления ${result}`);
+
+/*Task2*/
+// let userNumber1 = +prompt('Введите первое число'); //+ преобразует строку в число
+// let userNumber2 = Number(prompt('Введите второе число'));
+// let result = (userNumber1 + userNumber2) / 2; //"4" + "4"/2 = 2 ->42
+// //alert(`Результат вычисления ${result}`);
+// alert((userNumber1 + userNumber2) / 2);
+
+/*Task3*/
+/*Task4*/
+// const MILESINKILOMETRS = 0.621371;
+// let userKilometrsNumber = Number(prompt('Введите второе число'));
+// let result = (userKilometrsNumber * MILESINKILOMETRS);
+// alert(`Результат вычисления ${result}`);
+
+/*Task5*/
+// let userNumber1 = +prompt('Введите первое число');
+// let userNumber2 = Number(prompt('Введите второе число'));
+// //let result = (userNumber1 + userNumber2); 
+// let result = (userNumber1 - userNumber2); 
+// alert(`Результат вычисления ${result}`);
+
+/*Task8*/
+let div = 5 % 2;
+let userNumber = +prompt('Введите трехзначное число');
+
+
+
