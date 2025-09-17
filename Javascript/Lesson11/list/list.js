9c34454e2e909df5ca27ff33fc99dda22b157c76
wtfutsc-lang <wtfutsc@gmail.com>
2025-09-17 21:47:09 +0300

    add Lesson12 (Events)

--- Javascript/Lesson11/list/list.js
+++ Javascript/Lesson11/list/list.js
@@ -13,130 +13,143 @@
 //     ul.append(li);
 // }
 
-
 /* task pz5  Создайте дерево из объекта.
 Напишите функцию createTree, которая создаёт вложенный список ul/li из объекта.*/
 
-// let data = {
-//     "Рыбы": {
-//       "форель": {},
-//       "лосось": {}
-//     },
-  
-//     "Деревья": {
-//       "Огромные": {
-//         "секвойя": {},
-//         "дуб": {}
-//       },
-//       "Цветковые": {
-//         "яблоня": {},
-//         "магнолия": {}
-//       }
-//     }
-//   };
+let data = {
+  Рыбы: {
+    форель: {},
+    лосось: {},
+  },
 
-// let div = document.getElementById("root");
-// let ul = document.createElement("ul");
+  Деревья: {
+    Огромные: {
+      секвойя: {},
+      дуб: {},
+    },
+    Цветковые: {
+      яблоня: {},
+      магнолия: {},
+    },
+  },
+};
 
-// // создаёт дерево в контейнере:
-// function createTree(container, data){
+let div = document.getElementById("root");
+let ul = document.createElement("ul");
 
-//     let ul = document.createElement("ul");
+// создаёт дерево в контейнере:
+function createTree(container, data) {
+  let ul = document.createElement("ul");
 
-//     for (let key in data) {                                 // for in возьмет ключи объектов
-//         //console.log(key);
-//         let li = document.createElement("li");
-//         li.innerText = key;
+  for (let key in data) {
+    // for in возьмет ключи объектов
+    //console.log(key);
+    let li = document.createElement("li");
+    li.innerText = key;
 
-//         if(data[key]){
-//             let ulInner = document.createElement("ul");
+    if (data[key]) {
+      let ulInner = document.createElement("ul");
 
-//             for(let key1 in data[key]){
-//                 let li1 = document.createElement("li");
-//                 li1.innerText = key1;
+      for (let key1 in data[key]) {
+        let li1 = document.createElement("li");
+        li1.innerText = key1;
 
-//                 if(data[key][key1]){
-//                     let ulSuperInner = document.createElement("ul");
+        if (data[key][key1]) {
+          let ulSuperInner = document.createElement("ul");
 
-//                     for(let key2 in data[key][key1]){
-//                         let li2 = document.createElement("li");
-//                         li2.innerText = key2;
+          for (let key2 in data[key][key1]) {
+            let li2 = document.createElement("li");
+            li2.innerText = key2;
 
-//                         ulSuperInner.append(li2);
-//                     }
-//                     li1.append(ulSuperInner);
-//                 }
+            ulSuperInner.append(li2);
+          }
+          li1.append(ulSuperInner);
+        }
 
-//                 ulInner.append(li1);
-//             }
+        ulInner.append(li1);
+      }
 
-//             li.append(ulInner);
-//         }
-//         //container.append(li);
-//         ul.append(li);
-//     } 
-//     container.append(ul);
-// };
+      li.append(ulInner);
+    }
+    //container.append(li);
+    ul.append(li);
+  }
+  container.append(ul);
+}
 
-// createTree(div, data);
+createTree(div, data);
 
-//let container = document.getElementById('container');
+let container = document.getElementById("container");
 
-
-//3! = 1 * 2 * 3
+// 3! = 1 * 2 * 3
 // function faktorial(n){
-
 // }
-
 // console.log(faktorial(3));
 
+/* с рекурсией: */
+function newCreateTree(container, data) {
+  let ul = document.createElement("ul");
+
+  for (let key in data) {
+    let li = document.createElement("li");
+    li.innerText = key;
+
+    if (data[key]) {                  // не пустой объект
+      newCreateTree(li, data[key]);
+    }
+    ul.append(li);
+  }
+  container.append(ul);
+}
+
+newCreateTree(div, data);
 
 /* Task */
-function showNotification(options){
-    let div = document.createElement("div");
+// function showNotification(options){
+//     let div = document.createElement("div");
 
-    div.classList.add("notification");
+//     div.classList.add("notification");
 
-    if(options.className){
-        div.classList.add(options.className);
-    }
+//     if(options.className){
+//         div.classList.add(options.className);
+//     }
 
-    div.style.top = options.top + "px";
-    div.style.right = options.right + "px";
-    div.innerText = options.html;
-    document.body.prepend(div);
-    //setTimeout( function(){div.remove()}, 1500);
+//     div.style.top = options.top + "px";
+//     div.style.right = options.right + "px";
+//     div.innerText = options.html;
+//     document.body.prepend(div);
+//     //setTimeout( function(){div.remove()}, 1500);
 
-    setTimeout(function(){
-        div.classList.add("hide");
-        setTimeout(function(){div.remove()},1000);
-    }, 1500);
-}
+//     setTimeout(function(){
+//         div.classList.add("hide");
+//         setTimeout(function(){div.remove()},1000);
+//     }, 1500);
+// }
 
-showNotification({
-    top: 10, // 10px от верхней границы окна (по умолчанию 0px)
-    right: 10, // 10px от правого края окна (по умолчанию 0px)
-    html: "Hello!", // HTML-уведомление
-    // className: "welcome" // дополнительный класс для div (необязательно)   
-    //html: "Ошибка! Вы ввели неверные данные", // HTML-уведомление
-    //className: "error" // дополнительный класс для div (необязательно)   
-});
+// showNotification({
+//     top: 10, // 10px от верхней границы окна (по умолчанию 0px)
+//     right: 10, // 10px от правого края окна (по умолчанию 0px)
+//     html: "Hello!", // HTML-уведомление
+//     // className: "welcome" // дополнительный класс для div (необязательно)
+//     //html: "Ошибка! Вы ввели неверные данные", // HTML-уведомление
+//     //className: "error" // дополнительный класс для div (необязательно)
+// });
 
-function notify(){
-    let top = randomInteger(0, 400);
-    let right = randomInteger(0, 600);
+// function notify(){
+//     let top = randomInteger(0, 400);
+//     let right = randomInteger(0, 600);
 
-    showNotification({
-        top: top, 
-        right: right,  
-        html: "Ошибка! Вы ввели неверные данные", // HTML-уведомление
-        className: "error" // дополнительный класс для div (необязательно)   
-    });
-}
+//     showNotification({
+//         top: top,
+//         right: right,
+//         html: "Ошибка! Вы ввели неверные данные", // HTML-уведомление
+//         className: "error" // дополнительный класс для div (необязательно)
+//     });
+// }
 
-function randomInteger(min, max){
-    let rand = min + Math.random() * (max + 1 - min);
-    return Math.floor(rand);
-}
+// function randomInteger(min, max){
+//     let rand = min + Math.random() * (max + 1 - min);
+//     return Math.floor(rand);
+// }
 
-setInterval(notify, 1000);
+// setInterval(notify, 1000);
---
+++ Javascript/Lesson12 (Events)/events.js
@@ -0,0 +1,128 @@
+// function show1(num1, num2){
+//     if(num1 < 0){
+//         return;
+//     }else{
+//         console.log(`Числа ${num1} и ${num2}`);
+//         show1(num1 - 1, num2 - 1);
+//     }
+// }
+// // function show2(num1, num2){
+// //     show1(num1, num2);
+// // }
+
+// // show2(2,3);
+
+// show1(2, 3);
+
+// function showNumbers(start, end){
+// //     for(let i = start; i <= end; i++){
+// //         console.log(i);
+// //     }
+//        if (start > end){
+//            return;
+//        }else{
+//            console.log(start);
+//            showNumbers(start + 1, end);
+//        }
+// }
+// showNumbers (0, 10);
+
+// function changeLinkText(event){
+//     let a = document.querySelector("a");
+
+//     a.innerText = "New link";
+
+//     console.dir(event);  // вывод объекта события
+// }
+
+// let button = document.querySelector("button");
+
+// // button.onclick = changeLinkText; // скобки использовать нельзя, потому что тогда будет вызов функции
+
+// // button.onclick = function(){   // вывод нельзя навесить больше одного обработчика
+// //     document.body.style.backgroundColor = 'red';
+// // }
+
+// button.addEventListener('click', changeLinkText );
+// button.addEventListener('click', function(){   
+//     document.body.style.backgroundColor = 'red';
+//     });
+// button.addEventListener('click', function(){   
+//     document.body.style.backgroundColor = 'pink';
+//     });
+// button.addEventListener('click', function(){   
+//     alert('Ready!');
+//     });
+
+// button.onclick = function(){
+//     alert(this.innerText);
+// }  
+
+// button.onclick = function(event){
+//     console.dir(event);
+// }  
+
+
+// button.onclick = function(event){
+//     console.dir(event.target);     // event.target = this
+// }
+
+
+// button.removeEventListener('click', changeLinkText);  // удаляем слушатель событий
+
+// не работает:
+// button.removeEventListener('click', function(){   
+//     document.body.style.backgroundColor = 'pink';
+//     });
+
+
+//button.onclick = null;  // удалить назначенные события
+
+// console.dir(button);
+
+
+
+/* Task 1 */
+// Добавьте JavaScript к кнопке button,
+// чтобы при нажатии элемент <div id="text"> исчезал.
+// 
+// function hideText(event){
+
+//     let div = document.getElementById("text");
+
+//     div.textContent = '';
+
+//     console.dir(event);
+// }
+
+// let button = document.querySelector("button");
+// button.addEventListener('click', hideText);
+
+
+
+// variant:
+// let button = document.querySelector("button");
+
+// button.addEventListener('click', function(){
+//     let div = document.getElementById("text");
+//         div.remove();
+// });
+
+// button.addEventListener('click', function(){
+//     this.hidden = true;
+// })
+
+const ball = document.querySelector(".ball");
+
+function moveBall(event){
+    //console.log(event);
+    let clientX = event.clientX;
+    let clientY = event.clientY;
+
+    //console.dir(ball);
+
+    ball.style.left = clientX - Math.floor(ball.offsetWidth/2) + "px";
+    ball.style.top = clientY - Math.floor(ball.offsetHeight/2) + "px";
+}
+
+document.body.addEventListener('click', moveBall);
