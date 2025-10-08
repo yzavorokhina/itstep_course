f2e0beb87b97f20c7e0ba95e826135209874e630
yzavorokhina <wtfutsc@gmail.com>
2025-10-08 21:47:03 +0300

    update Template7 (Artifacts)

--- CSS/Tamplate7_Artifacts/js/custom.js
+++ CSS/Tamplate7_Artifacts/js/custom.js
@@ -2,7 +2,24 @@ $(document).ready(function(){
     let options = {
         items: 4,
         margin: 30,
-    }
+        // nav: true,
+        dots:false,
+        // navContainer: '.slider-nav',
+        // navText: ["Влево", "Вправо"],
 
-    $(".owl-carousel").owlCarousel(options);
+        
+    }
+    // $(".owl-carousel").owlCarousel(options);
+
+    let owl = $('.owl-carousel');
+    owl.owlCarousel(options);
+
+    $("#slider-left").click(function(){
+        owl.trigger('prev.owl.carousel');
+    });
+
+    $("#slider-right").click(function(){
+        owl.trigger('prev.owl.carousel');
+    });
+
   });
