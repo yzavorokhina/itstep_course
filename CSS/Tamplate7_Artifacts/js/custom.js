a7ca80006ace44d186432276f47bce1e3243e018
yzavorokhina <wtfutsc@gmail.com>
2025-10-13 21:47:28 +0300

    update Template7

--- CSS/Tamplate7_Artifacts/js/custom.js
+++ CSS/Tamplate7_Artifacts/js/custom.js
@@ -1,14 +1,24 @@
+let  totalPages = 0;
+
+let currentPageIndex = 1;
+
+function setSliderPagination(){
+    $("#slider-pagination").html(`${currentPageIndex} из ${totalPages}`);
+}
+
+
 $(document).ready(function(){
     let options = {
         items: 4,
         margin: 30,
         // nav: true,
         dots:false,
+        slideBy : 4,
         // navContainer: '.slider-nav',
-        // navText: ["Влево", "Вправо"],
-
-        
+        // navText: ["Влево", "Вправо"], 
     }
+
+
     // $(".owl-carousel").owlCarousel(options);
 
     let owl = $('.owl-carousel');
@@ -16,10 +26,32 @@ $(document).ready(function(){
 
     $("#slider-left").click(function(){
         owl.trigger('prev.owl.carousel');
+
+        if(currentPageIndex > 1) {
+            currentPageIndex--;
+        }
+
+        setSliderPagination();
     });
 
     $("#slider-right").click(function(){
         owl.trigger('prev.owl.carousel');
+
+        if(currentPageIndex < totalPages) {
+            currentPageIndex++;
+        }
+
+        setSliderPagination();
     });
 
+    let totalCards = $(".owl-item").length; 
+
+    totalPages = Math.ceil(totalCards / options.items);
+
+    setSliderPagination();
+
   });
+
+  function validate(){
+      return true;
+  }
