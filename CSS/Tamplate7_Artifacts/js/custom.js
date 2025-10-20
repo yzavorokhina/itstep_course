eb5321b354c36c805e9a7d8b1b8ee11a8d14f5e1
yzavorokhina <wtfutsc@gmail.com>
2025-10-20 21:49:37 +0300

    add validations for forms Template7

--- CSS/Tamplate7_Artifacts/js/custom.js
+++ CSS/Tamplate7_Artifacts/js/custom.js
@@ -30,7 +30,6 @@ $(document).ready(function(){
         if(currentPageIndex > 1) {
             currentPageIndex--;
         }
-
         setSliderPagination();
     });
 
@@ -40,18 +39,67 @@ $(document).ready(function(){
         if(currentPageIndex < totalPages) {
             currentPageIndex++;
         }
-
         setSliderPagination();
     });
 
     let totalCards = $(".owl-item").length; 
-
     totalPages = Math.ceil(totalCards / options.items);
-
     setSliderPagination();
 
+    // $("#form-login").validate();
+
+    const loginForm = document.querySelector('#form-login');
+
+    if(loginForm){
+        loginForm.addEventListener('submit', function(event){
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+            this.classList.add('was-validated')
+        });
+    }
   });
 
   function validate(){
-      return true;
+  
+    //   event.preventDefault();
+    //   event.stopPropogation();
+      let isValid = true;
+      let form = document.forms[0];
+
+      let name = form.elements[0];
+      let email = form.elements[1];
+
+    //   let str = "Hi! My name is Julie. I'm 1 5 7 3 3 years old."
+    //   console.dir(document.forms);
+
+        // let regex = /\sJulie/;
+        // let regex = /\d{2,}/; 
+        // let regex = /\d\s\d\s\d\s/; 
+        // let regex = /[A-Za-zА-Яа-я]{2,20}/; 
+
+        let name_regex = /^[A-Za-zА-Яа-я]{2,20}\s[A-Za-zА-Яа-я]{0,20}$/;
+        // console.dir(regex);
+        let email_regex = /^[a-z_\-0-9]+@[a-z_\-0-9]+\.[a-z]{2,3}$/;
+
+        name.classList.remove('is-invalid');
+        if(!name_regex.test(name.value)){
+            name.classList.add('is-invalid');
+            isValid = false;
+        } 
+
+        email.classList.remove('is-invalid');
+        if(!email_regex.test(email.value)){
+            email.classList.add('is-invalid');
+            isValid = false;
+        }
+
+        // let result = regex.test(str);
+        // // if(result){
+        // //     console.log(result);
+        // // }
+        // console.dir(result);
+
+      return isValid;
   }
