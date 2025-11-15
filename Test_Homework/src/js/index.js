8821c164b975d847efe1505643e39bdf3afe2a43
Yuliya Zavorokhina <orangemagicpot@gmail.com>
2025-11-15 23:07:51 +0300

    update Test_Homework

--- Test_Homework/src/js/index.js
+++ Test_Homework/src/js/index.js
@@ -20,19 +20,48 @@ document.body.appendChild(component());
 $('body').css({ backgroundColor: 'red' });
 
 let currentColor;
-const colors = ['red', 'green', 'blue', 'cyan', 'magenta'];
+const colors = ['red', 'orange', 'yellow', 'green', 'cyan', 'navy', 'violet'];
 
-function changeBackground() {
+// function changeBackground() {
+//   const currentColorIndex = colors.indexOf(currentColor);
+//   const nextColor = colors[currentColorIndex + 1] || colors[0];
+
+//   document.body.style.background = nextColor;
+//   console.log(`new color is ${nextColor}`);
+//   currentColor = nextColor;
+// }
+// setInterval(changeBackground, 2000);
+// changeBackground('red');
+
+// get random color:
+// console.log(colors.length);
+// const randomElement = colors[Math.floor(Math.random() * colors.length)];
+// console.log(randomElement); 
+
+// get random color by function:
+// function getRandomElement(colors) {
+//   return colors[Math.floor(Math.random() * colors.length)];
+// }
+// const randomColor = getRandomElement(colors);
+// console.log(randomColor);
+
+// get random color with lodash:
+// const randomElement = _.sample(colors);
+// console.log(randomElement);
+
+function changeRandomBackground() {
   const currentColorIndex = colors.indexOf(currentColor);
-  const nextColor = colors[currentColorIndex + 1] || colors[0];
+  let nextColor = colors[currentColorIndex + 1] || colors[0];
+  // get random color with lodash:
+  const randomColor1= _.sample(colors);
+  const randomColor2 = colors[Math.floor(Math.random() * colors.length)];
+  nextColor = randomColor2;
 
   document.body.style.background = nextColor;
   console.log(`new color is ${nextColor}`);
   currentColor = nextColor;
 }
-
-setInterval(changeBackground, 2000);
-// changeBackground('red');
+setInterval(changeRandomBackground, 2000);
 
 // alertModule.myFunction('Это алерт!!');
 // showAlert('Hello');
