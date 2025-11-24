0ceaf52fa360e04b647a8157527fea58f3d24150
Yuliya Zavorokhina <orangemagicpot@gmail.com>
2025-11-24 16:29:14 +0300

    update AJAX_Profanity_Checker_API

--- AJAX_Profanity_Checker_API/src/js/index.js
+++ AJAX_Profanity_Checker_API/src/js/index.js
@@ -2,69 +2,43 @@ import $ from 'jquery';
 import '../scss/custom.scss';
 import _ from 'lodash';
 
-let conn = new XMLHttpRequest();
+let formData = "";
+let checker;
 
-//XPathResult.open(method, URL) - инициализация
-//conn.open('GET', 'https://punkapi.online/v3/beers/random');
+window.sendMessage = async function(event) {
+    event.preventDefault();
+    formData = document.getElementById('myForm1Text').value;
+    checker = document.getElementById('myChecker');
+    console.log({formData});
 
-//xhr.send([body]) for POST
-// conn.send();
-//xhr.getResponseHeader
+    let con = new XMLHttpRequest();
+    con.open('POST', 'https://vector.profanity.dev');
+    con.setRequestHeader( 'Content-Type', 'application/json');
+    con.responseType = 'json';
 
-//получение ответа:
-//load/error/progress
+    let result;
 
-conn.onerror = function(){
-    console.log(`Не могу соединиться с целевым сервером`);
-}
+    con.onreadystatechange = function() {
+        if (con.readyState === 4) {
+            if (con.status === 200) {
+                console.log('Успех:', con.response);
+                result = con.response;
+                if(result.isProfanity == true){
+                    let h4 = '<h4> Bad word was detected! </h4>';
+                    checker.innerHTML = h4;
+                }
+                if(result.isProfanity == false){
+                    let h4 ='<h4> Now that was a normal word. </h4>';
+                    checker.innerHTML = h4;
+                }
+            } else {
+                console.error('Ошибка:', con.statusText);
+            }
+        }
+    };
 
-conn.onload = function(){
-    if(this.status !== 200) {
-        console.log(`Получен статус ответа ${this.status}. Ошибка!`);
-        return;
+    con.onerror = function(){
+        console.log(`Не могу соединиться с целевым сервером`);
     }
-    console.log(this.response);
-    renderBeerCard(this.response);
-}
-
-// функция смены фона:
-let currentColor;
-const colors = ['#7fff00', '#49d100', '#5ce000', '#6ef000', '#7fff00', '#90ff27', '#a0ff3d', '#6ef000', '#b1ff4f'];
-
-function changeRandomBackground() {
-  const currentColorIndex = colors.indexOf(currentColor);
-  let nextColor = colors[currentColorIndex + 1] || colors[0];
-  // get random color with lodash:
-  const randomColor1= _.sample(colors);
-  // const randomColor2 = Math.floor(Math.random() * (colors.length - 0 + 1)) + 0;
-  nextColor = randomColor1;
-  // console.log({randomColor2});
-
-  document.body.style.background = nextColor;
-  console.log(`new color is ${nextColor}`);
-  currentColor = nextColor;
-}
-
-setInterval( () => {
-    conn.open('GET', 'https://punkapi.online/v3/beers/random');
-    conn.responseType = 'json';
-    conn.send();
-    changeRandomBackground();
-}, 5000);
-
-// функция отрисовки карточки с продуктом:
-function renderBeerCard(randomBeer){
-    console.log(randomBeer);
-
-    const productCard = $(".product-card");
-    const { image, name, description } = randomBeer;
-
-    let beer_html = `<div>
-                        <img src="https://punkapi.online/v3/images/${image}" alt="${name}">
-                        <div>
-                            <h5>${name}</h5>
-                            <p>${description}</p>
-                        </div>
-                    </div>`;
-    productCard.html(beer_html); 
+    con.send(JSON.stringify({ message: formData } ));
 }
