34b35ee4c464d24932806dd717a081c159be996c
Yuliya Zavorokhina <orangemagicpot@gmail.com>
2026-01-11 23:08:49 +0300

    add Homework11 jQuery Modal Window

--- 4_Javascript/Homework11_jQuery_ModalWindow/modalWindow_with_jQuery.js
+++ 4_Javascript/Homework11_jQuery_ModalWindow/modalWindow_with_jQuery.js
@@ -2,3 +2,27 @@
 // modalWindow on bootstrap at the click of a button, 
 // with darkening around,
 // with_jQuery
+
+//выбираем все теги с именем  modal
+$('a[name=modal]').click(function (e) {
+    //Отменяем поведение ссылки
+    e.preventDefault();
+    //Получаем тег A
+    var divId = $(this).attr('href');
+    console.log({ divId });
+    var id = $(divId);
+    //Получаем ширину и высоту окна
+    var winH = $(window).height();
+    var winW = $(window).width();
+    //Устанавливаем всплывающее окно по центру
+    id.css('top', winH / 2 - id.height() / 2);
+    id.css('left', winW / 2 - id.width() / 2);
+    //эффект перехода
+    id.fadeIn(500);
+});
+//если нажата кнопка закрытия окна
+$('.modalwindow .close').click(function (e) {
+    //Отменяем поведение ссылки
+    e.preventDefault();
+    $('.modalwindow').fadeOut(500);
+});
