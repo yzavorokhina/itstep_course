05e826f836f76d4c9e7182b76d2441e9ec6647f0
yzavorokhina <orangemagicpot@gmail.com>
2026-02-11 21:48:47 +0300

    add 6_TypeScript Lesson

---
+++ 6_TypeScript/hangman_in_ts/src/js/index.ts
@@ -0,0 +1,71 @@
+// import '../scss/index.scss';
+
+// // let n = 5;
+// // // //  n = '4';
+// // console.log(n);
+
+// let n: number;
+
+// let city: string | null = "Питер";
+
+// let result: boolean = true;
+
+// let nothing = null;
+
+// city = null;
+
+// let m: number | undefined;
+
+// let some: any;
+
+// let color: "red" | "green" | "blue";
+
+// // color = "gray";
+
+// let ststus: 200 | 404 | 500;
+
+// type Person = {
+//     name: string,
+//     age: number,
+//     isStudent : boolean
+// };
+
+// let user: Person = {
+//     name: 'Иван Иванович',
+//     age: 35,
+//     isStudent: false
+// }
+
+// // user = {
+// //     111: 123344
+// // }
+
+// let numbers: number[] = [1, 2, 3, 4, 5];
+// let numbers2: Array<number> = [1, 2, 3, 4, 5];
+// let strings: Array<string|number> = ["12312", "3132", '4456', 1];
+// let strings2: string[] = ["12312", "3132", '4456', ];
+
+// function showInConsole(str: string): void{
+//     console.log (str);
+// }
+
+// function summ(m: number, n:number): number{
+//     return m + n;
+// }
+
+// function createPerson(name: string, age:number, isStudent: boolean): Person{
+//     return {
+//         name: name,
+//         age: age,
+//         isStudent: isStudent
+//     }
+// }
+
+// function throwError(message: string):never {
+//     throw new Error(message);
+// };
+
+// let obj: object;
+// obj = {name: "John", age: 30};
+
+
