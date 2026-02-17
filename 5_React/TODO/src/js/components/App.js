af7e4fec95007392fba133e57860a55f45a73d70
Yuliya Zavorokhina <orangemagicpot@gmail.com>
2026-02-17 17:02:27 +0300

    add styles for TODO in React

--- 5_React/TODO/src/js/components/App.js
+++ 5_React/TODO/src/js/components/App.js
@@ -13,15 +13,10 @@ export default function App() {
 
   function addNewTask(event) {
     event.preventDefault();
-    /**
-     * {id: 123123, text: "Задача 1", status: 1}
-     */
-    // const task = {
-    //   id: uid(),
-    //   text: text,
-    //   status: 0,
-    // };
-
+    if (!text || text.trim().length === 0 || text === "") {
+      alert("Пожалуйста заполните описание задачи");
+      return;
+    }
     const newTasks = [...tasks, { id: uid(), text: text, status: false }];
     setTasks(newTasks);
     console.log(tasks);
@@ -62,49 +57,52 @@ export default function App() {
 
   return (
     <>
-      {/* <H1 headerText={"ToDo"} /> */}
-      {/* <H1 className="header">ToDo</H1> */}
-      <H1>ToDo</H1>
-      <form action="" className="form" onSubmit={addNewTask}>
-        <input
-          type="text"
-          placeholder="Новая задача..."
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
-        <button type="submit" className="button">
-          Добавить
-        </button>
-      </form>
+      <div className="wrapper">
+        {/* <H1 headerText={"ToDo"} /> */}
+        {/* <H1 className="header">ToDo</H1> */}
+        <H1>ToDo</H1>
+        <form action="" className="form" onSubmit={addNewTask}>
+          <input
+            type="text"
+            placeholder="Новая задача..."
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <button type="submit" className="button">
+            Добавить
+          </button>
+        </form>
 
-      {tasks.length > 0 ? (
-        <ul className="list">
-          {tasks.map(function (task) {
-            return (
-              <li className="item" key={task.id}>
-                <input
-                  type="checkbox"
-                  checked={task.status}
-                  value="1"
-                  onChange={() => {
-                    changeStatus(task.id);
-                  }}
-                />
-                <span className={task.status ? "done" : null}>{task.text}</span>
-                <button
-                  onClick={() => {
-                    deleteTask(task.id);
-                  }}
-                >
-                  ❌
-                </button>
-              </li>
-            );
-          })}
-        </ul>
-      ) : (
-        "Список задач пустой"
-      )}
+        {tasks.length > 0 ? (
+          <ul className="list">
+            {tasks.map(function (task) {
+              return (
+                <li className="item" key={task.id}>
+                  <input
+                    type="checkbox"
+                    checked={task.status}
+                    value="1"
+                    onChange={() => {
+                      changeStatus(task.id);
+                    }}
+                  />
+                  <span className={task.status ? "done" : null}>{task.text}</span>
+                  <button
+                    onClick={() => {
+                      deleteTask(task.id);
+                    }}
+                  >
+                    ❌
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
+        ) : (
+          "Список задач пустой"
+        )}
+
+      </div>
     </>
   );
 }
